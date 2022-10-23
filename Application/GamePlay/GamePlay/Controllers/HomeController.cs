f7ca92d458613a2340176eb808804a06dcdca929
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2022-10-23 15:03:34 +0300

    Добавлено представление для страницы новостей

--- Application/GamePlay/GamePlay/Controllers/HomeController.cs
+++ Application/GamePlay/GamePlay/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace GamePlay.Controllers
             return View();
         }
 
+        public IActionResult News()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
