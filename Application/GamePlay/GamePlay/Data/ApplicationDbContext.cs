dc98e7a77e5c7f3cb0ab13c05f50ceee19bddbd8
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-10-23 11:53:12 +0300

    Bug FIx

--- Application/GamePlay/GamePlay/Data/ApplicationDbContext.cs
+++ Application/GamePlay/GamePlay/Data/ApplicationDbContext.cs
@@ -17,7 +17,7 @@ namespace GamePlay.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql(
-                "server=sql11.freesqldatabase.com;user=sql11528557;password=hA3bRIYxcE;database=sql11528557;",
+                "",
                 new MySqlServerVersion(new Version(8, 0, 11))
             );
         }
